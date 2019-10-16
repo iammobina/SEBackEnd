f7477279daa03fae29e5b355e99e4589c275495d
Mobina Kashanian <Mobina.km@gmail.com>
2019-10-16 14:28:22 +0330

    Add APIView of Creating a Place.

--- Places/views.py
+++ Places/views.py
@@ -1,2 +1,11 @@
 from django.shortcuts import render
+from rest_framework.generics import CreateAPIView
 # Create your views here.
+
+
+class CreatePlaceAPIView(CreateAPIView):
+    queryset = Places.objects.all()
+    serializer_class = CreatePlaceSerializer
+
+    def perform_create(self, serializer):
+        serializer.save()
